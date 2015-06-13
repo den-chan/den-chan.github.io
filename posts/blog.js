32d11d7327bcaf58a7afb643a43377fd3413521b
den-chan <den-chan@tuta.io>
2015-06-13 12:39:17 +1000

    Added blog.

---
+++ posts/blog.js
@@ -0,0 +1,48 @@
+init()
+
+function $ (a) { return document.getElementById(a) }
+function write (raw) {
+  //  **bold**  //italic//  ``monospaced``  [[link url]]  [[link url|link text]]
+  //  >quote (\>no quote)  ␣␣↵line break  ↵↵paragraph  #(#..)header# #timestamp header||#
+  function escape (text) {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode(text));
+    return div.innerHTML
+  }
+  var r = escape(raw), newline = r.indexOf("\r\n") !== -1 ? "\r\n" : r.indexOf("\n") !== -1 ? "\n" : "";
+  r = r
+    .replace(new RegExp(" + " + newline, "g"), "<br />" + newline)
+    .replace(/(#{1,5})(((?!\|\|).)*)?(\|\|[^#]*)?#/g, function ($0, $1, $2, _, $3, i) {
+      var ix = i + $1.length + ($2 = $2 || "").length + 2;
+      if ($3 === "||") {
+        $("body").value = $("body").value.slice(0, ix) + new Date(Date.now()).toISOString() + $("body").value.slice(ix + $3.length - 2, Infinity);
+        $("body").setSelectionRange(ix, ix);
+      }
+      var l = $1.length + 1;
+      if (l === 2) {
+        var slug = encodeURI( $2.replace(/\s+/g, "-").replace(/\*\*(.*)\*\*/g, "$1").replace(/``(.*)``/g, "$1")
+          .replace(/^/," ").replace(/([^:])\/\/(((?!\/\/)[\s\S])*)\/\//gm, "$2").replace(/^ /,"")
+          .replace(/\[\[([^\]|]*)\]\]/g, "$1").replace(/\[\[([^|]*)\|([^\]]*)\]\]/g, "$2")
+          .replace(/[^\w\-.~!$&'()*+,;=:@]/g, "").toLowerCase() );
+        return "<header><a class='hashlink' id='" + slug + "' href='#" + slug + "'></a><h2 class='title'>" + $2 +
+          "</h2><time class='timestamp'>" + (($3 && $3.slice(2, Infinity)) || new Date(Date.now()).toISOString()) + "</time></header>"
+      } else return "<h" + l + ">" + $2 + "</h" + l + ">"
+    })
+    .replace(/``([\s\S]*)``/g, "<code>$1</code>") //incorrect. redo as textNode
+    .replace(/\*\*([\s\S]*)\*\*/g, "<strong>$1</strong>")
+    .replace(/^/," ").replace(/([^:])\/\/(((?!\/\/)[\s\S])*)\/\//gm, "$1<em>$2</em>").replace(/^ /,"")
+    .replace(/\[\[([^\]|]*)\]\]/g, function ($0, $1) { return "<a href='" + $1.replace(/'/g, "&apos;") + "'>" + $1 + "</a>" })
+    .replace(/\[\[([^|]*)\|([^\]]*)\]\]/g, function ($0, $1, $2) { return "<a href='" + $1.replace(/'/g, "&apos;") + "'>" + $2 + "</a>" });
+  for (var p = (newline === "" ? [r] : r.split(newline + newline)), i = 0; i < p.length; i++) {
+    p[i] = p[i]
+      .replace(/^&gt;(.*)/g, "<blockquote>$1</blockquote>").replace(/^\\&gt;/g, "&gt;")
+      .replace(/^( +)/g, function (match) { return match.replace(/\s/g,"&nbsp;") });
+    p[i] = p[i].replace(/([\s\S]+)/g, "<p>$1</p>")
+  }
+  document.getElementById("blog").innerHTML = p.join(newline);
+  var
+    hashes = document.querySelectorAll("#render a.hashlink"),
+    titles = document.querySelectorAll("#render .title"),
+    i = hashes.length;
+  while (i--) hashes[i].style.height = titles[i].clientHeight + "px"
+}
---
+++ posts/generate.js
@@ -0,0 +1,27 @@
+function init () {
+  function em (id) { return window.getComputedStyle($(id)).fontSize.slice(0, -2) }
+  function addEvents (obj) {
+    for (var id in obj) for (var e in obj[id]) {
+      var el = id ? $(id) : window, a = e.split(" "), b = a.length;
+      while (b--) el.addEventListener(a[b], obj[id][e].bind(el), false)
+    }
+  }
+  function resize () { 
+    $("form").style.height = $("render").style.height = this.innerHeight + "px";
+    $("body").style.minHeight = this.innerHeight - .6 * em("body") - 3 + "px";
+  }
+  function generate () {
+    var offset = $("render").scrollTop ? $("render").scrollHeight - $("render").scrollTop : Infinity;
+    $("blog").innerHTML = "";
+    write($("body").value);
+    $("render").scrollTop = $("render").scrollHeight - offset;
+    var offset = $("form").scrollTop ? $("form").scrollHeight - $("form").scrollTop : Infinity;
+    $("body").style.height = "auto";
+    $("body").style.minHeight = "";
+    $("body").style.height = $("body").scrollHeight - .6 * em("body") + 2 + "px";
+    $("form").scrollTop = $("form").scrollHeight - offset
+    resize()
+  }
+  addEvents({ body: { change: generate, "cut paste drop keydown": function () { window.setTimeout(generate, 0) } }, "": { resize: resize } });
+  generate()
+}
